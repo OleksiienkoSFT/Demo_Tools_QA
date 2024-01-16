b03d800911a08c20797b9624638802ab549427fa
OleksiienkoSFT <vs.alex84@gmail.com>
2024-01-16 14:06:15 +0200

    Update AuthTests

--- AQA-23-api-tests/src/main/java/userData/TestUsers.java
+++ AQA-23-api-tests/src/main/java/userData/TestUsers.java
@@ -4,7 +4,7 @@ public class TestUsers {
     // public static final String BASE_USER_NAME = "ValeraUser1";
     // public static final String BASE_USER_PASS = "P@ssword2";
     public static final String BASE_USER_NAME = "StasUser1";
-    public static final String BASE_USER_PASS = "P@ssword1";
-    public static final String BASE_USER_ID = "367c56e4-4541-a651-c1d883ba8fb5";
+    public static final String BASE_PASS = "P@ssword1";
+    public static final String BASE_USER_ID = "367c56e4-be31-4541-a651-c1d883ba8fb5";
 
 }
--- AQA-23-api-tests/src/test/java/apiTests/AuthTests.java
+++ AQA-23-api-tests/src/test/java/apiTests/AuthTests.java
@@ -1,12 +1,21 @@
 package apiTests;
 
 import baseRequestConstructor.BaseConstructor;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertTrue;
+import static userData.TestUsers.BASE_USER_ID;
 
 public class AuthTests extends BaseConstructor {
     @Test
     public void getUserBooksAfterAuth(){
-        String token = getTokenForBaseUser();
+        String token = getTokenForBaseUser(); // Get token for user
         System.out.println("INFO:" + token);
+        ExtractableResponse<Response> resp = getBooksForUser(token, BASE_USER_ID);
+        assertTrue(resp.body().jsonPath().get("books.isbn[0]").equals("9781449325862"));
+        assertThat(resp.body().jsonPath().getList("books.isbn"),
+                allOf(hasItem("9781449325862"),hasItem("9781449337711")));
     }
 }
